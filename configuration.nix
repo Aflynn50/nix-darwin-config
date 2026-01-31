4c3d2ef485d9861e4f175cb778a82f5194b77b0f
Alastair Flynn <af@aflynn.uk>
2026-01-31 13:11:45 +0000

    feat: start adding config for new laptop and refactoring

--- configuration.nix
+++ configuration.nix
@@ -1,29 +1,6 @@
 
 { pkgs, ... }: {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages =
-    [ pkgs.vim
-    ];
-
-  # Necessary for using flakes on this system.
-  nix.settings.experimental-features = "nix-command flakes";
-
-  # Enable fish shell integration. This needs to be done here as well as in
-  # home manager. Enabling it here populates the path with nix locations.
-  programs.fish.enable = true;
-
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 6;
-
-  # The platform the configuration will be used on.
-  nixpkgs.hostPlatform = "aarch64-darwin";
-
-  users.users.alasflyn = {
-      name = "alasflyn";
-      home = "/Users/alasflyn";
-      shell = pkgs.fish;
-  };
-  home-manager.users.alasflyn = ./home/home.nix;
+  imports = [
+    ./modules/darwin/configuration.nix
+  ];
 }
---
+++ modules/darwin/configuration.nix
@@ -0,0 +1,31 @@
+
+{ pkgs, ... }: {
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [ pkgs.vim
+    ];
+
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Enable fish shell integration. This needs to be done here as well as in
+  # home manager. Enabling it here populates the path with nix locations.
+  programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 6;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  # Let Determinate Nix handle Nix configuration
+  nix.enable = false;
+
+  # Enable touch ID for sudo
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+}
